93b5a67255abcedb90ff080a394a67b2791558b3
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2024-11-22 15:48:05 +0100

    Fix my.h

--- include/my.h
+++ include/my.h
@@ -15,4 +15,11 @@
     #include <dirent.h>
     #include <sys/stat.h>
 
+// Lib
+int get_len(const char *str);
+int get_array_len(char **array);
+char *my_strdup(char *src, char to_insert);
+char **my_arraydup(char **src, char *to_insert);
+int write_mess(char *mess);
+int write_error(char *mess);
 #endif
